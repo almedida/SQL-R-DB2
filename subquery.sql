625a50f3b67dc011864b239cf77f441ca69cb5eb
Hammed Lawal <almedida@gmail.com>
2021-10-27 11:29:34 -0500

    Create subquery.sql

---
+++ subquery.sql
@@ -0,0 +1,17 @@
+--compare all employee salaries with average salary
+SELECT EMP_ID, F_NAME, L_NAME, SALARY, 
+(SELECT AVG(SALARY) AS "AVG_SALARY" FROM EMPLOYEES)
+  FROM EMPLOYEES;
+  
+--all employees records whose salary is lower than the average salary and compare
+SELECT EMP_ID, F_NAME, L_NAME, SALARY, 
+(SELECT AVG(SALARY) AS "AVG_SALARY" FROM EMPLOYEES)
+  FROM EMPLOYEES
+  WHERE SALARY <(SELECT AVG(SALARY) FROM EMPLOYEES);
+ 
+
+--all employees records with EMP_ID, SALARY and 
+--maximum salary as MAX_SALARY
+SELECT EMP_ID, SALARY,
+(SELECT MAX(SALARY) AS "MAX_SALARY" FROM EMPLOYEES)
+FROM EMPLOYEES;
