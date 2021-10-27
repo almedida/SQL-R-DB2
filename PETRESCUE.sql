9d1de88ff5c1c912533231c58ff24bf72600ad86
Hammed Lawal <almedida@gmail.com>
2021-10-27 09:33:39 -0500

    Update PETRESCUE.sql

--- PETRESCUE.sql
+++ PETRESCUE.sql
@@ -36,16 +36,62 @@ AS "TOTAL_NO_OF_ANIMALS"
   
 --the total cost of all animal rescues in the PETRESCUE table
 SELECT SUM(COST) AS "TOTAL_COST_OF ANIMALS"
-FROM PETRESCUE;
+  FROM PETRESCUE;
 
 --the maximum quantity of animals rescued
 SELECT MAX(QUANTITY) AS "MAX_QTY_OF_ANIMALS_RESCUED"
-FROM PETRESCUE;
+  FROM PETRESCUE;
 
 -- the average cost of animals rescued
 SELECT AVG(COST) AS "AVG_COST"
-FROM PETRESCUE;
+  FROM PETRESCUE;
 
 --the average cost of rescuing a dog
 SELECT AVG(COST/QUANTITY) AS "AVG_COST_PER DOG"
-FROM PETRESCUE WHERE ANIMAL ='Dog';
+  FROM PETRESCUE
+  WHERE ANIMAL ='Dog';
+
+--the rounded cost of each rescue
+SELECT ROUND(COST) "ROUNDED_COST"
+  FROM PETRESCUE;
+
+--the length of each animal name
+SELECT ANIMAL, LENGTH(ANIMAL) "LENGTH_OF_NAME"
+  FROM PETRESCUE;
+
+--the animal name in each rescue in uppercase
+SELECT UPPER(ANIMAL)
+  FROM PETRESCUE;
+
+--the animal name in each rescue in uppercase without duplications
+SELECT DISTINCT(UPPER(ANIMAL)) AS "ANIMAL"
+  FROM PETRESCUE;
+
+--all the columns from the PETRESCUE table, where the animal(s) 
+--rescued are cats. Use cat in lower case in the query
+SELECT *
+  FROM PETRESCUE
+  WHERE LOWER(ANIMAL) LIKE LOWER('%CAT%');
+
+--the day of the month when cats have been rescued.
+SELECT ANIMAL, DAY(RESCUEDATE) AS "DAY_RESCUED"
+  FROM PETRESCUE
+  WHERE UPPER(ANIMAL) LIKE UPPER('%cat%');
+
+--the number of rescues on the 5th month
+SELECT SUM(QUANTITY) AS "TOTAL_RESCUE_ON_5TH_MONTH"
+  FROM PETRESCUE
+  WHERE MONTH(RESCUEDATE) = '05';
+
+-- the number of rescues on the 14th day of the month.
+SELECT SUM(QUANTITY) AS "TOTAL_RESCUED ON 14TH DAY"
+  FROM PETRESCUE
+  WHERE DAY(RESCUEDATE) = '14';
+
+--the third day from each rescue.
+SELECT RESCUEDATE + 3
+  FROM PETRESCUE;
+
+--the length of time the animals have been rescued
+SELECT (CURRENT_DATE - RESCUEDATE) AS "RESCUE_LENGTH (YMMDD)"
+  FROM PETRESCUE;
